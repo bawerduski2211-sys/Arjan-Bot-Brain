8cba45090b027df8fc74c7fe8d1338222f350fd5
bawerduski2211-sys <bawerduski2211@gmail.com>
2026-01-31 02:22:01 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,15 +1,8 @@
 from telebot import types
 
 def main_keyboard():
-    # دروستکرنا مێنیۆیا پێشکەفتی ب شێوازێ ستوونی
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    
-    # پێناسا دوگمەیان ب ناڤێن جوان و سەرنجڕاکێش
-    btn1 = types.InlineKeyboardButton("🎤 دانوستاندنا دەنگی یا هەڤالینی", callback_data="mode_voice")
-    btn2 = types.InlineKeyboardButton("🎨 وێنەیێن 3D & 4K Ultra", callback_data="mode_image")
-    btn3 = types.InlineKeyboardButton("📸 ستۆدیۆیا دیزاینا کەسی", callback_data="mode_studio")
-    
-    # زێدەکرنا دوگمەیان بۆ مێنیۆیێ
-    markup.add(btn1, btn2, btn3)
-    
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    itembtn1 = types.KeyboardButton('🤖 دەربارەی من')
+    itembtn2 = types.KeyboardButton('💫 هاریکاری')
+    markup.add(itembtn1, itembtn2)
     return markup
