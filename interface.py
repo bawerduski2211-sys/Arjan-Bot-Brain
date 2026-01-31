7c38f5a8a939a308ea0751fca705938f4a643ce2
bawerduski2211-sys <bawerduski2211@gmail.com>
2026-01-31 05:42:11 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ from telebot import types
 
 def main_keyboard():
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    itembtn1 = types.KeyboardButton('🤖 دەربارەی من')
+    itembtn1 = types.KeyboardButton('🤖 دەربارەی Arjan AI')
     itembtn2 = types.KeyboardButton('💫 هاریکاری')
     markup.add(itembtn1, itembtn2)
     return markup
